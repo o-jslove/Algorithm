b6cbef6be231e6c9541f99a025164d66a0c1ab39
Minseok Lee <gksflaxkdns12@gmail.com>
2024-05-11 02:37:54 +0900

    Create B2743.java

---
+++ BaekJoon/B2743.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2743 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String st = br.readLine();
+		br.close();
+		
+		System.out.println(st.length());
+	}
+}
