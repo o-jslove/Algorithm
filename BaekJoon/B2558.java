9d3078d463fc8864925ed0e439f30d9f6be50d30
Minseok Lee <gksflaxkdns12@gmail.com>
2024-05-11 03:35:51 +0900

    Create B2558.java

---
+++ BaekJoon/B2558.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B2558 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.println(a + b);
+	}
+
+}
